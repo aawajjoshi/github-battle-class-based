d76d8dcf37a947ba318a7730a4f466d94bb8327a
Aawaj Joshi <s521315@nwmissouri.edu>
2020-10-14 16:49:14 -0500

    Create Nav Component

--- app/index.js
+++ app/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import Popular from './components/Popular';
+
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello</h1>
+            <div>
+                <Popular />
+            </div>
         )
     }
 }
